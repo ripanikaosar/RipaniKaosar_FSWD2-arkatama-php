7c8b0e6ba79e51a3bb18cfe2e3fa98853b9e38a3
ripanikaosar <ripanikaosar@gmail.com>
2023-05-10 22:43:21 +0700

    Upload projek php

---
+++ tugasphp4/koneksidb.php
@@ -0,0 +1,14 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $dbname = 'arkatama_store';
+    
+    $connection = mysqli_connect($host, $user, $password, $dbname);
+
+    // if($connection){
+    //     echo "Berhasil";
+    // }else{
+    //     echo "Koneksi Gagal";
+    // }
+?>
